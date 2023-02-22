87beaebea1739b570901e34e79ea6afede79ceb3
Leo Volwahsen <volwahsenl@gmail.com>
2023-02-22 14:01:43 +0100

    components structure added and styling included in template

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,3 +1,5 @@
-export default function () {
-    return <></>;
+import React from "react";
+
+export default function Chat() {
+    return <div>Chat</div>;
 }
---
+++ src/components/Supliers/Supliers.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Supliers() {
+    return <div>Supliers</div>;
+}
