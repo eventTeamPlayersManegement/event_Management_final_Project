55023db6c538f85278d24fbdda6b3f87e80164bc
Marcelearn <jaworskimarcel@gmail.com>
2023-02-22 14:00:41 +0100

    context's app

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.scss";
+import Context from './context/Context'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Context>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Context>
   </React.StrictMode>
 );
