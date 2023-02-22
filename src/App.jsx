869f60d110ad83143f719d052c9dd4d4b2beef13
Vaso <vasothom@hotmail.com>
2023-02-22 20:06:53 +0100

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import navItems from "./content/data.json";
 import About from "./views/About.jsx";
 import { Route, Routes } from "react-router-dom";
 import Services from "./views/Services.jsx";
+import Header from "./components/Header/Index.jsx";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Event Management</h1>
+
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path={navItems.navItems[0]} element={<About />} />
--- src/views/About.jsx
+++ src/views/About.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import Header from "../components/Header/Index";
 
 function About() {
-    return (
-        <div>
-            <Header/>
-
-        </div>
-    );
+  return <div>HELLO</div>;
 }
 
 export default About;
