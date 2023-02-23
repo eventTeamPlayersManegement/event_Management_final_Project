0129d3c1b282fa8f94d2c2471188598d5de969b7
Vaso <vasothom@hotmail.com>
2023-02-23 12:36:48 +0100

    contact html erstellt

--- src/components/Contact/Index.jsx
+++ src/components/Contact/Index.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import contact from "../../content/data.json";
 
 export default function Contact() {
-    return <div>Contact</div>;
+  return (
+    <div>
+      <h1>{contact.contactUs[0]}</h1>
+      <form>
+        <div>
+          <input type="text" placeholder="Title" />
+        </div>
+        <div>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+        </div>
+        <div>
+          <button>Send</button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import Contact from "../components/Contact/Index.jsx";
 
-function Contact() {
+export default function () {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <Contact />
+    </div>
+  );
 }
-
-export default Contact
