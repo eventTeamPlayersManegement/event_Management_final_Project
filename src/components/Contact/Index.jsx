f96579a10b8d398e59f941fa92f4fa7035ca7744
Vaso <vasothom@hotmail.com>
2023-02-23 13:32:08 +0100

    component contact styled

--- src/components/Contact/Index.jsx
+++ src/components/Contact/Index.jsx
@@ -1,20 +1,21 @@
 import React from "react";
+import "./style.scss";
 import contact from "../../content/data.json";
 
 export default function Contact() {
   return (
-    <div>
+    <div className="form-container">
       <h1>{contact.contactUs[0]}</h1>
       <form>
-        <div>
-          <input type="text" placeholder="Title" />
-        </div>
-        <div>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-        </div>
-        <div>
-          <button>Send</button>
-        </div>
+        <input type="text" placeholder="Title" />
+
+        <textarea
+          id="textarea-field"
+          name="textarea-field"
+          placeholder="Send as your message:"
+        ></textarea>
+
+        <button type="submit">Send</button>
       </form>
     </div>
   );
