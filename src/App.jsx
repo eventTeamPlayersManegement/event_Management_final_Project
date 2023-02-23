ab2e6045bb08c83adafb232bed9210a98a38b142
Elena Rotari <elena.rotari@icloud.com>
2023-02-23 09:26:02 +0100

    routes done till now..if there will be some more routes ..we will add them

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ import navItems from "./content/data.json";
 import About from "./views/About.jsx";
 import { Route, Routes } from "react-router-dom";
 import Services from "./views/Services.jsx";
+import Suppliers from "./views/Suppliers.jsx";
+import Contact from "./views/Contact.jsx";
+import Login from "./views/Login.jsx";
+import Register from "./views/Register.jsx";
+import Chat from "./views/Chat.jsx";
+import Dashbord from "./views/Dashbord.jsx";
+import Page404 from "./views/Page404.jsx";
+import Invoice from "./views/Invoice.jsx";
+import Payment from "./components/Payment/Index.jsx";
 
 function App() {
   return (
@@ -13,6 +22,15 @@ function App() {
         <Route path="/" element={<Landing />} />
         <Route path={navItems.navItems[0]} element={<About />} />
         <Route path={navItems.navItems[1]} element={<Services />} />
+        <Route path={navItems.navItems[2]} element={<Suppliers />} />
+        <Route path={navItems.navItems[3]} element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/dashboard" element={<Dashbord />} />
+        <Route path="/invoice" element={<Invoice />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
   );
