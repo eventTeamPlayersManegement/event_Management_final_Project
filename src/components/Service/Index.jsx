bc221f3e7ab60965a7f663788553c1609bf99587
Vaso <vasothom@hotmail.com>
2023-02-27 18:15:22 +0100

    Services html erstellt

--- src/components/Service/Index.jsx
+++ src/components/Service/Index.jsx
@@ -1,5 +1,88 @@
 import React from "react";
-
+import "./style.scss";
+import { useState } from "react";
+import ServiceData from "../../content/data.json";
+import CustomButton from "../lib/CustomButton.jsx";
+const submit = "submit";
 export default function Service() {
-    return <div>Service</div>;
+  const [option, setOption] = useState("");
+
+  function handleOptionChange(event) {
+    setOption(event.target.value);
+  }
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log("submited");
+  };
+
+  return (
+    <>
+      <h1 className="title">{ServiceData.Services.title}</h1>
+      <form onClick={handleClick}>
+        <div className="input-select-container">
+          <label>Select an option:</label>
+          <select
+            id="option-select"
+            value={option.value}
+            onChange={handleOptionChange}
+          >
+            <option value={option.value}>
+              {ServiceData.Services.select.select1}
+            </option>
+            <option value={option.value}>
+              {ServiceData.Services.options.option1}
+            </option>
+            <option value={option.value}>
+              {ServiceData.Services.options.option2}
+            </option>
+          </select>
+          <select
+            id="option-select"
+            value={option.value}
+            onChange={handleOptionChange}
+          >
+            <option value={option.value}>
+              {ServiceData.Services.select.select2}
+            </option>
+            <option value={option.value}>Locations</option>
+          </select>
+          <select
+            id="option-select"
+            value={option.value}
+            onChange={handleOptionChange}
+          >
+            <option value={option.value}>
+              {ServiceData.Services.select.select3}
+            </option>
+            <option value={option.value}>Date</option>
+          </select>
+          <select
+            id="option-select"
+            value={option.value}
+            onChange={handleOptionChange}
+          >
+            <option value={option.value}>
+              {ServiceData.Services.select.select4}
+            </option>
+            <option value={option.value}>Space</option>
+          </select>
+          <select
+            id="option-select"
+            value={option.value}
+            onChange={handleOptionChange}
+          >
+            <option value={option.value}>
+              {ServiceData.Services.select.select5}
+            </option>
+            <option value={option.value}>Deckor</option>
+          </select>
+          <CustomButton
+            type="submit"
+            submit={ServiceData.Services.submit}
+            style={"customBtnDefault"}
+          />
+        </div>
+      </form>
+    </>
+  );
 }
