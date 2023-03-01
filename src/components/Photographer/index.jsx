1b64681597dd62629395c67ddeb0367cf70ccbd5
Marcelearn <jaworskimarcel@gmail.com>
2023-03-01 16:16:59 +0100

    map components Photograph und fotos

--- src/components/Photographer/index.jsx
+++ src/components/Photographer/index.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
-import { useState } from 'react';
 import { getPhotographer } from '../../library/api'
 const daten = await getPhotographer()
+console.log(daten)
 
 function index() {
-    const [data, setData] = useState(daten)
-    console.log(data)
+    // const [data, setData] = useState(daten)
+    const Photograph = (({photograph}) => {
+        return(
+            <>
+                <h2>{photograph.name}</h2>
+                {photograph.fotos.map((el, i) => <Images key={i} image={el}/>)}
+            </>
+        ) 
+    }) 
+    const Images = (({image})=> {
+        return (
+           
+            <div className='image'>
+                <p>{image.title}</p>
+                <img src={image.url} alt="image" />
+            </div>
+        )
+    })
 
   return (
-    <div>
-        <h2>{data[0].name}</h2>
-        <img src={data[0].fotos[0].url}></img>
-    </div>
+      <>
+        <h1>Photographers</h1>
+        {daten.map((el, i) => <Photograph key={i} photograph={el} />)}
+     </>
   )
 }
 
