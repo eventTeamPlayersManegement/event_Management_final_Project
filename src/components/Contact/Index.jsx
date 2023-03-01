143c67fec42f201d482dec58c54a0820c17b3814
Vaso <vasothom@hotmail.com>
2023-03-01 11:05:33 +0100

    contact finished

--- src/components/Contact/Index.jsx
+++ src/components/Contact/Index.jsx
@@ -1,17 +1,43 @@
 import React from "react";
 import "./style.scss";
 import contact from "../../content/data.json";
+import { useState } from "react";
 
 export default function Contact() {
+  const initial = {
+    title: "",
+    message: "",
+  };
+  const [values, setValues] = useState(initial);
+  const handlerInput = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(values);
+    setValues(initial);
+  };
   return (
     <div className="form-container">
       <h1>{contact.contactUs[0]}</h1>
-      <form>
-        <input type="text" placeholder="Title" />
+      <form onSubmit={onSubmitHandler}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          onChange={handlerInput}
+          value={values.title}
+        />
 
         <textarea
           id="textarea-field"
-          name="textarea-field"
+          name="message"
+          onChange={handlerInput}
+          value={values.message}
           placeholder="Send as your message:"
         ></textarea>
 
