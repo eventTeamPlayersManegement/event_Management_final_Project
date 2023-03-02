42b9b6bf91f64bdd9469a86f2976787e22c79706
Leo Volwahsen <volwahsenl@gmail.com>
2023-03-02 11:42:14 +0100

    updated price in invoice

--- src/components/Invoice/index.jsx
+++ src/components/Invoice/index.jsx
@@ -6,21 +6,16 @@ export default function () {
     const { h2, paragraph } = data.invoice;
     let names = paragraph.map((el) => (
         <li key={el.name} className="list-item">
-            {el.name} {el.name}
+            {el.name} {el.price}
         </li>
     ));
-    // let money = costs.map((el) => (
-    //     <li key={el} className="list-item">
-    //         {el}
-    //     </li>
-    // ));
+
     return (
         <section>
             <h2 className="heading">{h2[0]}</h2>
             <article className="article-invoice">
                 <h3>{h2[1]}</h3>
                 <ul>{names}</ul>
-                {/* <ul>{money}</ul> */}
             </article>
         </section>
     );
