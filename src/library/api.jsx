3fa74735ea7ca69470d88b41229d5ad72d7a2502
Elena Rotari <elena.rotari@icloud.com>
2023-03-02 13:27:18 +0100

    api get Path added to api.js

--- src/library/api.jsx
+++ src/library/api.jsx
@@ -1,16 +1,29 @@
 const baseUrl = "https://final-project-backend-lkvb.onrender.com/api";
 
+// export const getPhotographer = async () => {
+//     try {
+//         let urlGetAll = `${baseUrl}/photographer`
 
-export const getPhotographer = async () => {
-    try {
-        let urlGetAll = `${baseUrl}/photographer`
+//         const response = await fetch(urlGetAll);
+//         const data = await response.json();
 
-        const response = await fetch(urlGetAll);
-        const data = await response.json();
+//         return data;
+//     } catch (error) {
+//         console.error(error);
+//         return
+//     }
+// }
 
-        return data;
-    } catch (error) {
-        console.error(error);
-        return 
-    }
-}
+export const getPath = async (path) => {
+  try {
+    let urlGetAll = `${baseUrl}/${path}`;
+
+    const response = await fetch(urlGetAll);
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    return;
+  }
+};
