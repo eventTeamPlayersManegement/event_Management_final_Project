5f9a2fc9eac26aaeedfc7cf26d3658467ba1115f
Vaso <vasothom@hotmail.com>
2023-03-07 12:17:02 +0100

    entairtainment styled

--- src/components/Entertainments/index.jsx
+++ src/components/Entertainments/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getPath } from "../../library/api";
+import "./style.scss";
 // const database = await getPath("entertainment");
 // console.log(database);
 export default function index() {
@@ -13,15 +14,23 @@ export default function index() {
   }, []);
   const Entertainment = ({ entertainment }) => {
     return (
-      <>
-        <h1> {entertainment.name} </h1>
-        {entertainment.fotos.map((fotos) => (
-          <div key={fotos._id}>
-            <h2>{fotos.title}</h2>
-            <img src={fotos.url} alt="entertainment" />
-          </div>
-        ))}
-      </>
+      <div className="home-galerie__container">
+        {/* <h1> {entertainment.name} </h1> */}
+        <>
+          {entertainment.fotos.map((fotos) => (
+            <div className="photo-card__container--polaroid" key={fotos._id}>
+              <img
+                className="photo-card--image__item"
+                src={fotos.url}
+                alt="entertainment"
+              />
+              <div className="photo-card--image__text">
+                <p>{fotos.title}</p>
+              </div>
+            </div>
+          ))}
+        </>
+      </div>
     );
   };
   return (
