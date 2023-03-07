7e845c709c5532f7726b9b0b919a0af8b6d90de6
Marcelearn <jaworskimarcel@gmail.com>
2023-03-07 10:31:00 +0100

    restaurant fetch bug fixed

--- src/components/Restaurants/index.jsx
+++ src/components/Restaurants/index.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
 import { getPath } from "../../library/api";
-const database = await getPath("restaurant");
-// console.log(database);
+
 export default function index() {
+  const [database, setData] = useState([])
+  async function getData () {
+    const daten = await getPath("restaurant");
+    setData(daten);
+  };
+  useEffect(() => {
+      getData();
+  }, []);
   const Restaurant = ({ restaurant }) => {
     return (
       <>
