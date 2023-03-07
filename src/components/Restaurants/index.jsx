7f1250537d659ee1fcfc8270e7aa0733eb06a90f
Vaso <vasothom@hotmail.com>
2023-03-07 11:52:15 +0100

    restaurant styled

--- src/components/Restaurants/index.jsx
+++ src/components/Restaurants/index.jsx
@@ -1,38 +1,45 @@
 import React, { useState, useEffect } from "react";
 import "./style.scss";
 import { getPath } from "../../library/api";
+import text from "../../content/data.json";
 
 export default function index() {
-  const [database, setData] = useState([])
-  async function getData () {
+  const [database, setData] = useState([]);
+  async function getData() {
     const daten = await getPath("restaurant");
     setData(daten);
-  };
+  }
   useEffect(() => {
-      getData();
+    getData();
   }, []);
   const Restaurant = ({ restaurant }) => {
     return (
-      <>
-        <h1> {restaurant.name}</h1>
-        {/* {restaurant.createdAt}
-        {restaurant.updatedAt} */}
+      <div className="home-galerie__container">
+        {/* <h1> {restaurant.name}</h1> */}
+        <>
+          {restaurant.fotos.map((el) => (
+            <div className="photo-card__container--polaroid" key={el._id}>
+              <img
+                className="photo-card--image__item"
+                src={el.url}
+                alt="restaurant"
+              />
 
-        {restaurant.fotos.map((el) => (
-          <div key={el._id}>
-            <h2>{el.title}</h2>
-            <img src={el.url} alt="restaurant" />
-          </div>
-        ))}
-      </>
+              <div className="photo-card--image__text">
+                <p>{el.title}</p>
+              </div>
+            </div>
+          ))}
+        </>
+      </div>
     );
   };
   return (
-    <div>
-      <h1>Restaurants</h1>
+    <>
+      <h1>{text.restaurant.title} </h1>
       {database.map((el) => (
         <Restaurant key={el._id} restaurant={el} />
       ))}
-    </div>
+    </>
   );
 }
