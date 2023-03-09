78edb548fefa2f3c54fc8b62489dff5791802e40
Marcelearn <jaworskimarcel@gmail.com>
2023-03-09 14:38:50 +0100

    profilepicture added

--- src/components/Photographers/index.jsx
+++ src/components/Photographers/index.jsx
@@ -13,6 +13,7 @@ function Index() {
     useEffect(() => {
         getData();
     }, []);
+    console.log(data)
   const Photograph = ({ photograph }) => {
     return (
       <>
@@ -20,6 +21,16 @@ function Index() {
             {photograph.fotos.map((el, i) => (
                 <Images key={i} image={el} />
                 ))}
+
+        </div>
+      </>
+    );
+  };
+  const Profilepicture = ({ photograph }) => {
+    return (
+      <>
+        <div className="profile-picture__container">
+            <img className='profile-picture--image__item' src={photograph.profilepicture} alt="photograph" />
 
         </div>
       </>
@@ -39,6 +50,9 @@ function Index() {
   return (
     <>
       <h1>{text.photographer.title}</h1>
+      {data.map((el, i) => (
+        <Profilepicture key={i} photograph={el} />
+      ))}
       {data.map((el, i) => (
         <Photograph key={i} photograph={el} />
       ))}
