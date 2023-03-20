cfbbe1cea39a4f5f4d7ee142dae646cb8befa698
Elena Rotari <elena.rotari@icloud.com>
2023-03-20 15:47:32 +0100

    controls changed  video

--- src/components/Landing/Video.jsx
+++ src/components/Landing/Video.jsx
@@ -2,7 +2,7 @@ import video from "../../assets/video/video3.mp4";
 
 const Video = () => {
   return (
-    <video width="100%" height="920px" controls autoPlay muted replay>
+    <video width="100%" height="920px" autoPlay muted controls={false}>
       <source src={video} type="video/mp4" />
     </video>
   );
