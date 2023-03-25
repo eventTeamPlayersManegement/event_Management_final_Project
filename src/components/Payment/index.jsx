725a2e36c1ad12aa0cafaf1ff608f3d2f3e84148
Elena Rotari <elena.rotari@icloud.com>
2023-03-25 13:35:12 +0100

    98% services done with style and functionality

--- src/components/Payment/index.jsx
+++ src/components/Payment/index.jsx
@@ -2,20 +2,5 @@ import React from "react";
 import data from "../../content/data.json";
 import "./style.scss";
 export default function () {
-    const { h1, h2, paragraph } = data.payment;
-    return (
-        <>
-            <h1 className="page-title">{h1}</h1>
-            <p className="page-paragraph">{paragraph[0]}</p>
-            <p className="page-paragraph">
-                {paragraph[1]}: {paragraph[2]}
-            </p>
-            <br></br>
-            <h2 className="page-title">{h2}</h2>
-            <p className="page-paragraph">{paragraph[3]}</p>
-
-            <p className="page-paragraph">{paragraph[4]}</p>
-            
-        </>
-    );
+  return <div>Show t</div>;
 }
--- src/views/Payment.jsx
+++ src/views/Payment.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import Payment from "../components/Payment";
-import Button from "../components/Button";
 
 export default function () {
-    return (
-        <>
-            <Payment />
-            <Button label="Pay now" />
-        </>
-    );
+  return <Payment />;
 }
