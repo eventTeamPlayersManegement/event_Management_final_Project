bfa074b96318ad982593d1c649eb7df9c74daedf
Marcelearn <jaworskimarcel@gmail.com>
2023-03-28 11:05:23 +0200

    commentar deleted

--- src/components/Supliers/Index.jsx
+++ src/components/Supliers/Index.jsx
@@ -1,5 +1,3 @@
-// import { useState, useEffect } from "react";
-// import { getPath } from "../../library/api";
 import "./style.scss";
 import SuppliersNav from "../SuppliersNav";
 import Photographers from "../Photographers";
@@ -8,19 +6,6 @@ import Rentautos from "../Rentautos";
 import Entertainments from "../Entertainments";
 
 export default function Supliers() {
-  // const [data, setData] = useState([]);
-
-  // useEffect(() => {
-  //   async function getData() {
-  //     const data = await getPath("suppliers");
-  //     setData(data);
-  //   }
-  //   getData();
-  // }, []);
-
-  // const Images = ({ image }) => {
-  //   return <img style={{ width: "200px" }} src={image} alt="image" />;
-  // };
   return (
     <div className="ourSuppliers">
 
@@ -29,17 +14,7 @@ export default function Supliers() {
       <Entertainments />
       <Rentautos />
       <Restaurants />
-      {/* 
-      <div className="caruselContainer">
-        <div className="restaurants"></div>
-        <div className="entertainement"></div>
-        <div className="rentcar"></div>
-      </div>
-      <div className="mobile_grid_photos">
-        {data.map((el, id) => (
-          <Images key={id} image={el} />
-        ))}
-      </div> */}
+     
     </div>
   );
 }
