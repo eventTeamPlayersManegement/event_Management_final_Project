a5534a4a89f02dda604fa8f21528238228c811e6
Vaso <vasothom@hotmail.com>
2023-04-03 22:22:17 +0200

    about US and landing page ubersetzt

--- src/components/Landing/Index.jsx
+++ src/components/Landing/Index.jsx
@@ -5,8 +5,10 @@ import Next from "./Next.jsx";
 // import NextBtn from "./NextBtn.jsx";
 import "./style.scss";
 import Video from "./Video.jsx";
-
+import { useTranslation } from "react-i18next";
 export default function Landing() {
+  const { t } = useTranslation();
+
   return (
     <div className="landingPageContainer">
       <div className="video">
@@ -16,8 +18,8 @@ export default function Landing() {
         <img src={img1} alt="happycouple" />
       </div>
       <div className="moto">
-        <p>{data.landingText.wish}</p>
-        <p>{data.landingText.our}</p>
+        <p>{t("landingText.wish")}</p>
+        <p>{t("landingText.our")}</p>
       </div>
       <Next />
       <div className="landingImg">
---
+++ src/components/Switchlanguage/Index.jsx
@@ -0,0 +1,39 @@
+import i18n from "i18next";
+import { useTranslation, initReactI18next } from "react-i18next";
+
+import { Suspense } from "react";
+import data from "../../content/data.json";
+import translationDE from "../../content/dataDE.json";
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    resources: {
+      en: { translation: data },
+      de: { translation: translationDE },
+    },
+    lng: "en",
+    fallbackLng: "en",
+    interpolation: { escapeValue: false },
+  });
+
+function LanguageSwitcher() {
+  const { t } = useTranslation();
+  const onChange = (event) => {
+    i18n.changeLanguage(event.target.value);
+  };
+  return (
+    <Suspense fallback={"Loading..."}>
+      <div>
+        <header className="App-header">
+          {/* <h1>{t("welcome")}</h1>*/}
+          <select name="language" onChange={onChange}>
+            <option value="en">English</option>
+            <option value="de">German</option>
+          </select>
+        </header>
+      </div>
+    </Suspense>
+  );
+}
+
+export default LanguageSwitcher;
